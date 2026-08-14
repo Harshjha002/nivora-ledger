e220b45f160b9a8d53875a64e1e3e7545fb3da34
Harsh jha <harshjha8453@gmail.com>
2026-08-14 10:53:55 +0530

    feat: complete phase one production hardening

--- src/app.js
+++ src/app.js
@@ -12,8 +12,14 @@ const app = express();
 
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
-app.use(cors());
 app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
 
 app.get("/health", (req, res) => {
   return res.status(200).json({
