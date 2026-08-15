347b0b8778e6a22684d129ed33d379b7a7a3881d
Harsh jha <harshjha8453@gmail.com>
2026-08-15 14:06:51 +0530

    refactor: centralize application logging

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const env = require("./src/config/env");
 const app = require("./src/app");
+const logger = require("./src/config/logger");
 const {
     connectToDB,
     disconnectFromDB,
@@ -14,16 +15,16 @@ const startServer = async () => {
         await connectToDB();
 
         server = app.listen(env.PORT, "0.0.0.0", () => {
-            console.log(`Server is running on port ${env.PORT}`);
+            logger.info({ port: env.PORT }, "Server is running");
         });
     } catch (error) {
-        console.error("Failed to start server:", error);
+        logger.error({ err: error }, "Failed to start server");
         process.exit(1);
     }
 };
 
 const shutdown = async (signal) => {
-    console.log(`${signal} received. Starting graceful shutdown...`);
+    logger.info({ signal }, "Signal received. Starting graceful shutdown...");
 
     if (!server) {
         await disconnectFromDB();
@@ -32,17 +33,17 @@ const shutdown = async (signal) => {
 
     server.close(async (error) => {
         if (error) {
-            console.error("HTTP server shutdown failed:", error);
+            logger.error({ err: error }, "HTTP server shutdown failed");
             process.exit(1);
         }
 
-        console.log("HTTP server closed");
+        logger.info("HTTP server closed");
 
         try {
             await disconnectFromDB();
             process.exit(0);
         } catch (dbError) {
-            console.error("Graceful shutdown failed:", dbError);
+            logger.error({ err: dbError }, "Graceful shutdown failed");
             process.exit(1);
         }
     });
